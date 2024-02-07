b23791d006a4c626fe2544de23a8761d7e56ab6c
ESDLIFE\Melissat <melissatam@esdlife.com>
2024-02-07 12:09:47 +0800

    class

---
+++ packages/ui/src/utils/layoutOperator.ts
@@ -0,0 +1,48 @@
+export class LayoutOperator {
+  private selectionList: []
+  private componentsList: []
+  private propertiesList: []
+
+  constructor () {
+      this.selectionList = []
+      this.componentsList = []
+      this.propertiesList = []
+  }
+
+  getAllList () {
+      return {
+          selection: this.selectionList,
+          components: this.componentsList,
+          properties: this.propertiesList
+      }
+  }
+
+  getSelectionList () {
+      console.log('getSelectionList')
+      return this.selectionList
+  }
+
+  getComponentsList () {
+      return this.componentsList 
+  }
+
+  getPropertiesList () {
+      return this.propertiesList
+  }
+
+  registerSelectionList (element: string) {
+      // this.selectionList.push()
+      return this.selectionList
+  }
+
+  registerComponentsList (element: string) {
+      // this.selectionList.push()
+      return this.selectionList
+  }
+
+  registerPropertiesList (element: string) {
+      // this.selectionList.push()
+      return this.selectionList
+  }
+
+}
