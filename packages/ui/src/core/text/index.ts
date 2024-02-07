023ea66d86e3902657cd06b6fe064a77a80f20d3
ESDLIFE\Melissat <melissatam@esdlife.com>
2024-02-07 11:09:02 +0800

    update structure

---
+++ packages/ui/src/core/text/index.ts
@@ -0,0 +1,11 @@
+import TextSelectionJson from "./format/selection-panel.json"
+import TextDragDropJson from "./format/drag-drop-area.json"
+import TextPropertyJson from "./format/property.json"
+// import * as TextHook from "./hook"
+// import TextWidget
+
+export {
+    TextSelectionJson,
+    TextDragDropJson,
+    TextPropertyJson
+}
